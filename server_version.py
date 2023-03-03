228ba40d658ef820be98fd8f97e8ba0a6ef871c1
srinivasmaram2025 <125262782+srinivasmaram2025@users.noreply.github.com>
2023-03-03 21:12:48 +0530

    Create server_version.py

---
+++ server_version.py
@@ -0,0 +1,19 @@
+import jenkinsapi
+from jenkinsapi.jenkins import Jenkins
+
+def get_server_instance():
+    """
+    This function is intended to get server instance
+    """
+    jenkins_url = 'http://18.207.240.57:8080/'
+    # The below line is to authenticate Jenkins server
+    server = Jenkins(jenkins_url, username='akhil', password='')
+    print("server", server)
+    return server
+
+if _name_ == '__main__':
+    gf = get_server_instance()
+    jenkins_server_version = gf.version
+    print (jenkins_server_version)
+
+
